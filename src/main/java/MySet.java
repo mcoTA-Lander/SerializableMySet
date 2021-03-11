152b1cee3844f2d638fc7c19d44f18efcdba58c6
dDyman1 <63016873+dDyman1@users.noreply.github.com>
2021-03-10 21:54:16 -0500

    Update MySet.java

--- src/main/java/MySet.java
+++ src/main/java/MySet.java
@@ -1,21 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-import org.jetbrains.annotations.NotNull;
-
-import java.io.*;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.Set;
-
-/**
- *
- * @author dovdm
- * @param <T>
- */
 public class MySet {
 
     private void writeObject(ObjectOutputStream msWrite) throws IOException{
